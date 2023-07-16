2b0d09d25093412496909426ed4250ec71b71cd0
dcarmal <dcarmal@gmail.com>
2023-07-16 19:39:23 +0200

    Code cleanup and file renaming

--- environment_down.sh
+++ environment_down.sh
@@ -5,20 +5,20 @@
 # Date: 15.07.2023
 # Description: This script checks env variables and runs docker-compose down.
 #
-if ! test -f "./Configs/settings.env"; then
-    echo "The file Configs/settings.env does not exist"
+if ! test -f "./Docker_configs/settings.env"; then
+    echo "The file Docker_configs/settings.env does not exist"
     exit 1
 fi
 
-echo "Initializing with the contents of the file Configs/settings.env:"
-cat ./Configs/settings.env
+echo "Initializing with the contents of the file Docker_configs/settings.env:"
+cat ./Docker_configs/settings.env
 echo ""
 
-source ./Configs/settings.env
+source ./Docker_configs/settings.env
 
 if [ -z "$NGINX_\*" ] || [ -z "$NGINX_PORT" ] || [ -z "$\*_SUBNET" ]; then
-    echo "Required environment variables are missing in the file Configs/settings.env"
+    echo "Required environment variables are missing in the file Docker_configs/settings.env"
     exit 1
 fi
 
-docker-compose --env-file Configs/settings.env down
+docker-compose --env-file Docker_configs/settings.env down
--- environment_up.sh
+++ environment_up.sh
@@ -9,7 +9,7 @@
 # Variable section
 #
 # - Log files to clear
-log_files=('./docker-compose.log' './NGINX_files/varlognginx/access.log' './NGINX_files/varlognginx/error.log')
+log_files=('./docker-compose.log' './NGINX_files/Logs/access.log' './NGINX_files/Logs/error.log')
 #
 # End of variable section
 #
@@ -18,19 +18,19 @@ log_files=('./docker-compose.log' './NGINX_files/varlognginx/access.log' './NGIN
 # Script start
 #
 # Checking settings file presence, if not found, error exit
-if ! test -f "./Configs/settings.env"; then
-    echo "The file Configs/settings.env does not exist"
+if ! test -f "./Docker_configs/settings.env"; then
+    echo "The file Docker_configs/settings.env does not exist"
     exit 1
 fi
 
 # Showing settings
-echo "Initializing with the contents of the file Configs/settings.env:"
-cat ./Configs/settings.env
+echo "Initializing with the contents of the file Docker_configs/settings.env:"
+cat ./Docker_configs/settings.env
 echo ""
 
 # Checking dockerfile presence and reporting it
 DOCKERFILE_EXISTS=false
-for file in Configs/Dockerfile*; do
+for file in Docker_configs/Dockerfile*; do
     if test -f "$file"; then
         DOCKERFILE_EXISTS=true
         echo "Using Dockerfile: $file"
@@ -39,14 +39,14 @@ done
 
 # If there's no dockerfile, error exit
 if ! $DOCKERFILE_EXISTS; then
-    echo "No Dockerfile exists in the Configs directory"
+    echo "No Dockerfile exists in the Docker_configs directory"
     exit 1
 fi
 
 # Using settings file
-source ./Configs/settings.env
+source ./Docker_configs/settings.env
 if [ -z "$NGINX_\*" ] || [ -z "$NGINX_PORT" ] || [ -z "$\*_SUBNET" ]; then
-    echo "Required environment variables are missing in the file Configs/settings.env"
+    echo "Required environment variables are missing in the file Docker_configs/settings.env"
     exit 1
 fi
 
@@ -67,7 +67,7 @@ for arg in "$@"; do
 done
 
 # Launching containers and capturing output in log
-docker-compose --env-file Configs/settings.env up -d 2>&1 | awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }' | tee docker-compose.log
+docker-compose --env-file Docker_configs/settings.env up -d 2>&1 | awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }' | tee docker-compose.log
 
 # If an error is logged, warn the user and error exit
 if [ ${PIPESTATUS[0]} -ne 0 ]; then
@@ -77,7 +77,7 @@ else
   # If docker-compose is successfully executed then docker inspect is
   echo ""
   echo "Environment up:"
-  docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq -f "name=web_dock_")
+  docker inspect -f '{{.Name}} - {{range $key, $value := .NetworkSettings.Networks}}{{$key}}: {{$value.IPAddress}} {{end}}{{println ""}}' $(docker ps -aq -f "name=web_dock_")
   echo ""
 fi
 #
