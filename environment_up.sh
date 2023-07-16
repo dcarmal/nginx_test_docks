5924a7524baf7ce08682c6e7fd91afd878ba4abd
dcarmal <dcarmal@gmail.com>
2023-07-16 17:40:21 +0200

    Added clearlogs function to environment_up script

--- environment_up.sh
+++ environment_up.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
-#
+# -
 # Title: environment_up.sh
 # Author: David C.
 # Date: 15.07.2023
 # Description: This script sets up a docker environment
+# -
+#
+# Variable section
+#
+# - Log files to clear
+log_files=('./docker-compose.log' './NGINX_files/varlognginx/access.log' './NGINX_files/varlognginx/error.log')
+#
+# End of variable section
+#
+#
+#
+# Script start
 #
 # Checking settings file presence, if not found, error exit
 if ! test -f "./Configs/settings.env"; then
     echo "The file Configs/settings.env does not exist"
     exit 1
 fi
+
 # Showing settings
 echo "Initializing with the contents of the file Configs/settings.env:"
 cat ./Configs/settings.env
 echo ""
+
 # Checking dockerfile presence and reporting it
 DOCKERFILE_EXISTS=false
 for file in Configs/Dockerfile*; do
@@ -22,19 +36,39 @@ for file in Configs/Dockerfile*; do
         echo "Using Dockerfile: $file"
     fi
 done
+
 # If there's no dockerfile, error exit
 if ! $DOCKERFILE_EXISTS; then
     echo "No Dockerfile exists in the Configs directory"
     exit 1
 fi
+
 # Using settings file
 source ./Configs/settings.env
 if [ -z "$NGINX_\*" ] || [ -z "$NGINX_PORT" ] || [ -z "$\*_SUBNET" ]; then
     echo "Required environment variables are missing in the file Configs/settings.env"
     exit 1
 fi
+
+# Parameter search
+for arg in "$@"; do
+# Checking for parameter -clearlogs
+    if [ "${arg}" = "-clearlogs" ]; then
+        # Then log files found are cleared
+        for file in "${log_files[@]}"; do
+            if [[ -f ${file} ]]; then     # Verify its presence
+                echo "Log file found: ${file}"
+                rm ${file} && echo "Successfully deleted: ${file}" || echo "Failed deleting: ${file}"
+            else
+                echo "No encontrado: ${file}"
+            fi 
+        done
+    fi
+done
+
 # Launching containers and capturing output in log
 docker-compose --env-file Configs/settings.env up -d 2>&1 | awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }' | tee docker-compose.log
+
 # If an error is logged, warn the user and error exit
 if [ ${PIPESTATUS[0]} -ne 0 ]; then
     echo "An error occurred while executing docker-compose. See \"docker-compose.log\" for details." 1>&2
@@ -46,4 +80,5 @@ else
   docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq -f "name=web_dock_")
   echo ""
 fi
+#
 # End of script
